14de2706a2cb1c68683d67d1a9e031cd55dd289c
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-08 04:27:47 +0530

    REFACTOR: code formatted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,36 +6,36 @@ import { AiOutlineMenu } from "react-icons/ai";
 export default function Header() {
   const [isShowMenu, setIsShowMenu] = useState(false);
   return (
-    <div className='flex justify-between lg:justify-around items-center p-4'>
-      <div className='logo-container flex justify-start items-center text-2xl lg:text-6xl font-bold text-amber-600 gap-x-4 '>
-        <div className='logo relative bg-orange-400 rounded-full p-4 w-12 h-12'>
-          <Image src={"/images/logo.svg"} alt='logo' fill={true} />
+    <div className="flex justify-between lg:justify-around items-center p-4">
+      <div className="logo-container flex justify-start items-center text-2xl lg:text-6xl font-bold text-amber-600 gap-x-4 ">
+        <div className="logo relative bg-orange-400 rounded-full p-4 w-12 h-12">
+          <Image src={"/images/logo.svg"} alt="logo" fill={true} />
         </div>
         <h1>OGZ</h1>
       </div>
 
       {/* desktop menu */}
-      <div className='menu hidden lg:flex  text-center items-center p-66 '>
-        <ul className='flex justify-between items-center gap-x-24 text-xs lg:text-sm'>
+      <div className="menu hidden lg:flex  text-center items-center p-66 ">
+        <ul className="flex justify-between items-center gap-x-24 text-xs lg:text-sm">
           <li>
-            <Link href='/'>Home</Link>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href='/'>White Paper</Link>
+            <Link href="/">White Paper</Link>
           </li>
           <li>
-            <Link href='/'>Afiliate Program</Link>
+            <Link href="/">Afiliate Program</Link>
           </li>
           <li>
-            <button className='btn'>Connect Wallet</button>
+            <button className="btn">Connect Wallet</button>
           </li>
         </ul>
       </div>
 
       {/* mobile menu */}
-      <div className='mobile-menu relative lg:hidden text-end'>
+      <div className="mobile-menu relative lg:hidden text-end">
         <button
-          className='border-none text-3xl'
+          className="border-none text-3xl"
           onClick={() => setIsShowMenu(!isShowMenu)}
         >
           <AiOutlineMenu />
@@ -45,18 +45,18 @@ export default function Header() {
             isShowMenu ? "block" : "hidden"
           } text-center items-center bg-gray-900 absolute top-6 right-4 bottom-4`}
         >
-          <ul className='flex flex-col justify-between items-center gap-y-4 text-base'>
+          <ul className="flex flex-col justify-between items-center gap-y-4 text-base">
             <li>
-              <Link href='/'>Home</Link>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <Link href='/'>White Paper</Link>
+              <Link href="/">White Paper</Link>
             </li>
             <li>
-              <Link href='/'>Afiliate Program</Link>
+              <Link href="/">Afiliate Program</Link>
             </li>
             <li>
-              <button className='px-4 py-2 text-black font-bold bg-inherit text-white cursor-pointer rounded-md'>
+              <button className="px-4 py-2 text-black font-bold bg-inherit text-white cursor-pointer rounded-md">
                 Connect Wallet
               </button>
             </li>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className='hero w-3/4 lg:w-11/12 h-screen flex flex-col-reverse lg:flex-row justify-center items-center mx-auto'>
+    <div className="hero w-3/4 lg:w-11/12 h-screen flex flex-col-reverse lg:flex-row justify-center items-center mx-auto">
       {/* left side */}
-      <div className='w-3/4 flex flex-col justify-center items-start lg:items-start gap-y-4 p-6 pl-12'>
-        <h1 className='text-lg lg:text-4xl text-orange-500 font-bold leading-snug tracking-wide'>
+      <div className="w-full lg:w-3/4 flex flex-col justify-center items-start lg:items-start gap-y-4 p-6 pl-12">
+        <h1 className="text-2xl lg:text-4xl text-orange-500 font-bold leading-snug tracking-wide">
           OGZ coin, a coin for <br /> the community
         </h1>
-        <p className='w-full lg:w-3/4 text-start'>
+        <p className="w-full lg:w-3/4 text-start">
           OGZ is the utility token of the OGZ ecosystem. This coin is sure to
           attract investors who are looking for a fun and profitable investment
           opportunity. So saddle up, partner, and join the OGZ community today{" "}
           and become a real gang member!
         </p>
-        <button className='btn'>Read White Paper</button>
+        <button className="btn">Read White Paper</button>
       </div>
       {/* right side */}
-      <div className='w-3/4 h-3/4 lg:w-1/3 relative'>
-        <Image src='/images/heroImg.png' alt='hero' fill={true} />
+      <div className="w-3/4 h-4/5 lg:w-1/3 relative">
+        <Image src="/images/heroImg.png" alt="hero" fill={true} />
       </div>
     </div>
   );
