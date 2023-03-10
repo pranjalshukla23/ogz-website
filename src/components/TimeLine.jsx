aa3d128355b3899217924b083b223c0bf1d94f64
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-11 01:14:40 +0530

    REFACTOR: removed icons

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,5 +1,5 @@
 import { FaShareSquare, FaTelegramPlane } from "react-icons/fa";
-import { MdArrowDropDown } from "react-icons/Md";
+// import { MdArrowDropDown } from "react-icons/Md";
 import Image from "next/image";
 export default function TimeLine() {
   return (
@@ -13,11 +13,11 @@ export default function TimeLine() {
         </div>
         <div className="controls w-full md:w-1/2  flex flex-col md:flex-row gap-2 md:gap-4">
           <button className=" block w-full bg-amber-900 my-4 rounded p-2">
-            Sort <MdArrowDropDown className="inline text-3xl float-right" />
+            Sort 
           </button>
           <button className=" block w-full bg-amber-900 my-4 rounded p-2">
             Filter{" "}
-            <MdArrowDropDown className="inline text-3xl float-right" />
+           
           </button>
         </div>
       </div>
