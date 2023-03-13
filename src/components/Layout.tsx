b0914857295743c947bb5420349c942b842c868d
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-14 02:21:17 +0530

    FIX: provided height to header image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: Props) => {
       {children}
 
       {/* header gradient */}
-      <div className="bg-[url('/images/heading-background.svg')] bg-no-repeat bg-cover absolute top-0 bottom-3/4 right-0 left-0 "></div>
+      <div className="bg-[url('/images/heading-background.svg')] bg-no-repeat bg-cover h-[1867px] absolute top-0 bottom-3/4 right-0 left-0 "></div>
 
       {/* footer gradient */}
       <div className="bg-[url('/images/heading-background.svg')] bg-no-repeat absolute top-3/4 bottom-0 right-0 left-0 "></div>
