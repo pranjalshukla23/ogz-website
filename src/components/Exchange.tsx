97715fd945e62ba4a2724fcc19bba5300595a248
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-13 23:26:15 +0530

    FIX: background fix

--- src/components/Exchange.tsx
+++ src/components/Exchange.tsx
@@ -1,30 +1,28 @@
-import {
-  SupportedLocale,
-  SUPPORTED_LOCALES,
-  SwapWidget,
-  Theme,
-} from "@uniswap/widgets";
-import "@uniswap/widgets/fonts.css";
+//uniswap widgets library
+import { SwapWidget } from "@uniswap/widgets";
 
 export default function Exchange() {
-  const theme: Theme = {
-    primary: "#FFF",
-    secondary: "#A9A9A9",
-    interactive: "#000",
-    container: "#160B0A",
-    module: "rgba(217, 217, 217, 0.04)",
-    accent: "#E7862E",
-    outline: "#CC1",
-    dialog: "#000",
-    fontFamily: "Josefin Sans",
-    borderRadius: 0.5,
-  };
+  // const theme: Theme = {
+  //   primary: "#FFF",
+  //   secondary: "#A9A9A9",
+  //   interactive: "#000",
+  //   container: "#160B0A",
+  //   module: "rgba(217, 217, 217, 0.04)",
+  //   accent: "#E7862E",
+  //   outline: "#CC1",
+  //   dialog: "#000",
+  //   fontFamily: "Josefin Sans",
+  //   borderRadius: 0.5,
+  // };
 
   return (
     <div className='flex flex-col-reverse md:flex-row justify-start md:justify-between items-center mb-12 w-4/5  gap-6 md:gap-6'>
       {/* exchange form */}
 
-      <SwapWidget theme={theme} width={200} />
+      {/* <SwapWidget theme={theme} width={200} /> */}
+      {/* <div className='Uniswap'>
+        <SwapWidget />
+      </div> */}
 
       {/* right side  */}
       <div className='flex flex-col justify-start items-start gap-4 p-4 w-full md:w-2/3'>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,15 +5,15 @@ type Props = {
 };
 const Layout = ({ children }: Props) => {
   return (
-    <div className=' relative'>
+    <div className='relative'>
       <Header />
       {children}
 
       {/* header gradient */}
-      <div className="bg-[url('/images/heading-background.svg')] absolute top-0 bottom-3/4 right-0 left-0 "></div>
+      <div className="bg-[url('/images/heading-background.svg')] bg-no-repeat bg-right  absolute top-0 bottom-3/4 right-0 left-0 "></div>
 
       {/* footer gradient */}
-      <div className="bg-[url('/images/heading-background.svg')] absolute top-3/4 bottom-0 right-0 left-0"></div>
+      <div className="bg-[url('/images/heading-background.svg')] bg-no-repeat absolute top-3/4 bottom-0 right-0 left-0 "></div>
     </div>
   );
 };
