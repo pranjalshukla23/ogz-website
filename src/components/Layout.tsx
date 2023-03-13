1dee44b7d0215f1f66dea64174912d2de42e38b0
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-13 21:02:09 +0530

    ADD: added header and footer gradient

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,10 +5,16 @@ type Props = {
 };
 const Layout = ({ children }: Props) => {
   return (
-    <>
+    <div className='border-2 border-red-500 relative'>
       <Header />
       {children}
-    </>
+
+      {/* header gradient */}
+      <div className="bg-[url('/images/heading-background.svg')] absolute top-0 bottom-3/4 right-0 left-0 border-2"></div>
+
+      {/* footer gradient */}
+      <div className="bg-[url('/images/heading-background.svg')] absolute top-3/4 bottom-0 right-0 left-0"></div>
+    </div>
   );
 };
 
