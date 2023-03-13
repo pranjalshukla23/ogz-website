99dacfd3602b470808ab311695e19c27cee5c782
pranjalshukla23 <pranjalshukla23@gmail.com>
2023-03-14 01:52:07 +0530

    ADD: code refactored

--- src/components/Exchange.tsx
+++ src/components/Exchange.tsx
@@ -7,14 +7,6 @@ import {
   Theme,
 } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
-import { ethers } from "ethers";
-import { useState, useEffect } from "react";
-import { providers } from "ethers";
-import detectEthereumProvider from "@metamask/detect-provider";
-
-const jsonRpcUrlMap =
-  "https://goerli.infura.io/v3/17d501c2696c49e0ab34da8f4f1cfffd";
-const TOKEN_LIST = "https://tokens.uniswap.org/";
 
 const theme: Theme = {
   primary: "#FFF",
@@ -30,32 +22,10 @@ const theme: Theme = {
 };
 
 export default function Exchange() {
-  const [provider, setProvider] = useState<Web3Provider>();
-  const [account, setAccount] = useState({
-    address: "",
-    provider: provider,
-  });
-
-  // useEffect(() => {
-  //   let p = new ethers.providers.Web3Provider((window as any).ethereum) as any;
-  //   console.log(p);
-  //   setProvider(p);
-  // }, []);
-
-  async function connectWallet() {
-    console.log("running");
-  }
   return (
     <div className='flex flex-col-reverse md:flex-row justify-start md:justify-between items-center mb-12 w-4/5  gap-6 md:gap-6 '>
-      <button onClick={connectWallet}>Connect Wallet</button>
       {/* exchange form */}
-      <SwapWidget
-        provider={provider}
-        jsonRpcEndpoint={jsonRpcUrlMap}
-        tokenList={TOKEN_LIST}
-        theme={theme}
-        width={200}
-      />
+      <SwapWidget theme={theme} width={200} />
 
       {/* right side  */}
       <div className='flex flex-col justify-start items-start gap-4 p-4 w-full md:w-1/2 '>
